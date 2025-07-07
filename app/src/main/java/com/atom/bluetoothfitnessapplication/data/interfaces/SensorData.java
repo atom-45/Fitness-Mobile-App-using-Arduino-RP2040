4284a2006d8d96d27cdc638c2241ce924fe3c366
Matome <matomemodiba3@gmail.com>
2025-07-07 20:57:50 +0200

    initial commit

---
+++ app/src/main/java/com/atom/bluetoothfitnessapplication/data/interfaces/SensorData.java
@@ -0,0 +1,14 @@
+package com.atom.bluetoothfitnessapplication.data.interfaces;
+
+public interface SensorData {
+
+    int getId();
+    String getGyroX();
+    String getGyroY();
+    String getGyroZ();
+    String getAccX();
+    String getAccY();
+    String getAccZ();
+    String getTime();
+    String getDateOfExercise();
+}
